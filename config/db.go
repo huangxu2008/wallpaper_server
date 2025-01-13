c016cc6501b1ee940b61989340541a453d7b5ef5
huangxu <1324482835@qq.com>
2025-01-13 19:57:00 +0800

    数据库改为远程数据库

--- config/db.go
+++ config/db.go
@@ -1,5 +1,6 @@
 package config
 
 const (
-	Mysqldb = "root:123456@tcp(localhost:3306)/wallpaper?charset=utf8"
+	// Mysqldb = "root:123456@tcp(localhost:3306)/wallpaper?charset=utf8"
+	Mysqldb = "wallpaper:1q2w3e4rWallpaper!@#$@tcp(gserver1.guodongshell.com:19966)/wallpaper_test?charset=utf8"
 )
